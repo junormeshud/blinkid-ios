db93c919434373f541bc4f49670d05b0035b3200
Jurica Cerovec <jurica.cerovec@microblink.com>
2020-04-03 08:21:46 +0200

    Added UAE validation prototype

--- Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
+++ Samples/BlinkID-sample-Swift/BlinkID-sample-Swift/ViewController.swift
@@ -20,25 +20,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapScan(_ sender: AnyObject) {
-        
+        self.startScanWithDefaultUI()
+    }
+
+    func startScanWithDefaultUI() {
+
         /** Create BlinkID recognizer */
         self.blinkIdRecognizer = MBBlinkIdCombinedRecognizer()
         self.blinkIdRecognizer?.returnFullDocumentImage = true;
-        
+
         /** Create settings */
         let settings : MBBlinkIdOverlaySettings = MBBlinkIdOverlaySettings()
-        
+
         /** Crate recognizer collection */
         let recognizerList = [self.blinkIdRecognizer!]
         let recognizerCollection : MBRecognizerCollection = MBRecognizerCollection(recognizers: recognizerList)
-        
+
         /** Create your overlay view controller */
         let blinkIdOverlayViewController : MBBlinkIdOverlayViewController = MBBlinkIdOverlayViewController(settings: settings, recognizerCollection: recognizerCollection, delegate: self)
-        
+
         /** Create recognizer view controller with wanted overlay view controller */
         let recognizerRunneViewController : UIViewController = MBViewControllerFactory.recognizerRunnerViewController(withOverlayViewController: blinkIdOverlayViewController)
         recognizerRunneViewController.modalPresentationStyle = .fullScreen
-        
+
         /** Present the recognizer runner view controller. You can use other presentation methods as well (instead of presentViewController) */
         self.present(recognizerRunneViewController, animated: true, completion: nil)
     }
@@ -68,36 +72,89 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: MBBlinkIdOverlayViewControllerDelegate {
-    
+
+
     func blinkIdOverlayViewControllerDidFinishScanning(_ blinkIdOverlayViewController: MBBlinkIdOverlayViewController, state: MBRecognizerResultState) {
-        /** This is done on background thread */
+
+        // We use the guard to grab the result of the BlinkID Combined Recognizer
+        guard let blinkIdCombinedRecognizerResult = self.blinkIdRecognizer?.result else {
+            return
+        }
+
+        // We use another guard to check if the result state is valid. If not, we continue scanning
+        guard blinkIdCombinedRecognizerResult.resultState == MBRecognizerResultState.valid else {
+            return
+        }
+
+        /** We pause scanning to handle the UI after the successful scan */
         blinkIdOverlayViewController.recognizerRunnerViewController?.pauseScanning()
         
-        var message: String = ""
-        var title: String = ""
-        
-        if (self.blinkIdRecognizer?.result.resultState == MBRecognizerResultState.valid) {
-            title = "BlinkID"
-            
-            let fullDocumentImage: UIImage! = self.blinkIdRecognizer?.result.fullDocumentFrontImage?.image
-            print("Got BlinkID image with width: \(fullDocumentImage.size.width), height: \(fullDocumentImage.size.height)")
-            
-            // Save the string representation of the code
-            message = self.blinkIdRecognizer!.result.description
-            
-            /** Needs to be called on main thread beacuse everything prior is on background thread */
-            DispatchQueue.main.async {
-                // present the alert view with scanned results
-                
-                let alertController: UIAlertController = UIAlertController.init(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-                
-                let okAction: UIAlertAction = UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default,
-                                                                 handler: { (action) -> Void in
-                                                                    self.dismiss(animated: true, completion: nil)
-                })
-                alertController.addAction(okAction)
-                blinkIdOverlayViewController.present(alertController, animated: true, completion: nil)
-            }
+        if (!isDataMatching(blinkIdCombinedRecognizerResult)) {
+            // We want to show the message that the result data is not matching and instruct the user to scan again
+            showRepeatScanningAlert("Cannot match data from the front and the back", overlayViewController: blinkIdOverlayViewController)
+        } else if (!isDataValid(blinkIdCombinedRecognizerResult)) {
+            // We want to show the message that the result data cannot be validated and instruct the user to scan again
+            showRepeatScanningAlert("Cannot validate the data", overlayViewController: blinkIdOverlayViewController)
+        } else {
+            showSuccessfulResultAlert(blinkIdCombinedRecognizerResult, overlayViewController: blinkIdOverlayViewController)
+        }
+    }
+
+    func isDataMatching(_ result: MBBlinkIdCombinedRecognizerResult) -> Bool {
+
+        // PIN contains dashes, so we need to remove them before comparing it with the data from the back
+        let cleanedPIN = result.personalIdNumber?.replacingOccurrences(of: "-", with: "")
+
+        // to check whether the data matches, we just compare Personal ID number with OPT1 in the MRZ
+        return cleanedPIN == result.mrzResult.opt1
+    }
+
+    func isDataValid(_ result: MBBlinkIdCombinedRecognizerResult) -> Bool {
+
+        // We need to have a full name in the result
+        guard let fullName = result.fullName else {
+            return false
+        }
+
+        // Full name can contain just these characters
+        let nameCharset = CharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz- \n")
+
+        // If we got characters that are not in the above defined set, we cannot validate the data
+        return fullName.rangeOfCharacter(from: nameCharset.inverted) == nil
+    }
+
+    func showSuccessfulResultAlert(_ result: MBBlinkIdCombinedRecognizerResult, overlayViewController: MBBlinkIdOverlayViewController) {
+
+        /** Needs to be called on main thread */
+        DispatchQueue.main.async {
+            let alertController: UIAlertController = UIAlertController.init(title: "Success", message: result.description, preferredStyle: UIAlertController.Style.alert)
+
+            let okAction: UIAlertAction = UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default,
+                                                             handler: { (action) -> Void in
+                                                                self.dismiss(animated: true, completion: nil)
+            })
+            alertController.addAction(okAction)
+
+            overlayViewController.present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    func showRepeatScanningAlert(_ message: String, overlayViewController: MBBlinkIdOverlayViewController) {
+
+        /** Needs to be called on main thread */
+        DispatchQueue.main.async {
+
+            let alertController: UIAlertController = UIAlertController.init(title: "Scan unsuccessful", message: message, preferredStyle: UIAlertController.Style.alert)
+
+            let okAction: UIAlertAction = UIAlertAction.init(title: "Retry", style: UIAlertAction.Style.default,
+                                                             handler: { (action) -> Void in
+                                                                self.dismiss(animated: true) {
+                                                                    self.startScanWithDefaultUI()
+                                                                }
+            })
+            alertController.addAction(okAction)
+
+            overlayViewController.present(alertController, animated: true, completion: nil)
         }
     }
     
